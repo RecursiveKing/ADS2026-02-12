33f579dbd2028be3aed17033f15d0b4109944482
RecursiveKing <hostinginformationishere@gmail.com>
2026-09-20 23:57:51 +0300

    ADD ListA ready

--- src/by/it/group551001/bondarenko/lesson09/ListA.java
+++ src/by/it/group551001/bondarenko/lesson09/ListA.java
@@ -1,14 +1,17 @@
 package by.it.group551001.bondarenko.lesson09;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class ListA<E> implements List<E> {
 
     //Создайте аналог списка БЕЗ использования других классов СТАНДАРТНОЙ БИБЛИОТЕКИ
-
+    private static final int capacity = 10;
+    private Object[] elements;
+    private int size;
+    public ListA() {
+        this.elements = new Object[capacity];
+        this.size = 0;
+    }
     /////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////
     //////               Обязательные к реализации методы             ///////
@@ -16,22 +19,44 @@ public class ListA<E> implements List<E> {
     /////////////////////////////////////////////////////////////////////////
     @Override
     public String toString() {
-        return "";
+        if (size == 0) {
+            return "[]";
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for (int i = 0; i < size; i++) {
+            sb.append(elements[i] == this ? "(this Collection)" : elements[i]);
+            if (i < size - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
     @Override
     public boolean add(E e) {
-        return false;
+        ensureCapacity(size + 1);
+        elements[size++] = e;
+        return true;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        checkIndex(index);
+        @SuppressWarnings("unchecked")
+        E oldValue = (E)elements[index];
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(elements, index + 1, elements, index, numMoved);
+        }
+        elements[--size] = null;
+        return oldValue;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     /////////////////////////////////////////////////////////////////////////
@@ -42,100 +67,205 @@ public class ListA<E> implements List<E> {
 
     @Override
     public void add(int index, E element) {
-
+        checkIndexForAdd(index);
+        ensureCapacity(size + 1);
+        int numMoved = size - index;
+        if (numMoved > 0) {
+            System.arraycopy(elements, index, elements, index + 1, numMoved);
+        }
+        elements[index] = element;
+        size++;
     }
 
     @Override
     public boolean remove(Object o) {
+        int index = indexOf(o);
+        if (index >= 0) {
+            remove(index);
+            return true;
+        }
         return false;
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        checkIndex(index);
+        @SuppressWarnings("unchecked")
+        E oldValue = (E)elements[index];
+        elements[index] = element;
+        return oldValue;
     }
 
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elements[i] == null) return i;
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(elements[i])) return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        checkIndex(index);
+        @SuppressWarnings("unchecked")
+        E value = (E)elements[index];
+        return value;
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        return indexOf(o) >= 0;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        if (o == null) {
+            for (int i = size - 1; i >= 0; i--) {
+                if (elements[i] == null) return i;
+            }
+        } else {
+            for (int i = size - 1; i >= 0; i--) {
+                if (o.equals(elements[i])) return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        for (Object e : c) {
+            if (!contains(e)) return false;
+        }
+        return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        return false;
+        Object[] a = c.toArray();
+        int numNew = a.length;
+        if (numNew == 0) return false;
+        ensureCapacity(size + numNew);
+        for (int i = 0; i < numNew; i++) {
+            @SuppressWarnings("unchecked")
+            E e = (E)a[i];
+            elements[size++] = e;
+        }
+        return true;
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
-        return false;
+        checkIndexForAdd(index);
+        Object[] a = c.toArray();
+        int numNew = a.length;
+        if (numNew == 0) return false;
+        ensureCapacity(size + numNew);
+        int numMoved = size - index;
+        if (numMoved > 0) {
+            System.arraycopy(elements, index, elements, index + numNew, numMoved);
+        }
+        for (int i = 0; i < numNew; i++) {
+            @SuppressWarnings("unchecked")
+            E e = (E)a[i];
+            elements[index + i] = e;
+        }
+        size += numNew;
+        return true;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        boolean modified = false;
+        for (int i = 0; i < size; i++) {
+            if (c.contains(elements[i])) {
+                remove(i);
+                i--;
+                modified = true;
+            }
+        }
+        return modified;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        boolean modified = false;
+        for (int i = 0; i < size; i++) {
+            if (!c.contains(elements[i])) {
+                remove(i);
+                i--;
+                modified = true;
+            }
+        }
+        return modified;
     }
 
 
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
-        return null;
+        if (fromIndex < 0 || toIndex > size || fromIndex > toIndex) {
+            throw new IndexOutOfBoundsException("fromIndex: " + fromIndex + ", toIndex: " + toIndex + ", size: " + size);
+        }
+        ListA<E> sub = new ListA<>();
+        for (int i = fromIndex; i < toIndex; i++) {
+            @SuppressWarnings("unchecked")
+            E e = (E)elements[i];
+            sub.add(e);
+        }
+        return sub;
     }
 
     @Override
     public ListIterator<E> listIterator(int index) {
-        return null;
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("index: " + index);
+        }
+        return new ListAIterator(index);
     }
 
     @Override
     public ListIterator<E> listIterator() {
-        return null;
+        return new ListAIterator(0);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <T> T[] toArray(T[] a) {
-        return null;
+        if (a.length < size) {
+            return (T[]) java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size);
+        }
+        System.arraycopy(elements, 0, a, 0, size);
+        if (a.length > size) {
+            a[size] = null;
+        }
+        return a;
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] result = new Object[size];
+        System.arraycopy(elements, 0, result, 0, size);
+        return result;
     }
 
     /////////////////////////////////////////////////////////////////////////
@@ -146,7 +276,110 @@ public class ListA<E> implements List<E> {
     /////////////////////////////////////////////////////////////////////////
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new ListAIterator(0);
     }
 
+    private void ensureCapacity(int minCapacity) {
+        if (minCapacity > elements.length) {
+            int newCapacity = elements.length + (elements.length >> 1);
+            if (newCapacity < minCapacity) {
+                newCapacity = minCapacity;
+            }
+            Object[] newElements = new Object[newCapacity];
+            System.arraycopy(elements, 0, newElements,0, size);
+            elements = newElements;
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index< 0 || index >= size) {
+            throw new IndexOutOfBoundsException("index: " + index + ", size: " + size);
+        }
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("index: " + index + ", size: " + size);
+        }
+    }
+
+    private class ListAIterator implements ListIterator<E> {
+        private int cursor;
+        private int lastRet = -1;
+
+        ListAIterator(int index) {
+            this.cursor = index;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return cursor < size;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public E next() {
+            if (cursor >= size) {
+                throw new NoSuchElementException();
+            }
+            E next = (E)elements[cursor];
+            lastRet = cursor;
+            cursor++;
+            return next;
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return cursor > 0;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public E previous() {
+            if (cursor <= 0) {
+                throw new NoSuchElementException();
+            }
+            cursor--;
+            E prev = (E)elements[cursor];
+            lastRet = cursor;
+            return prev;
+        }
+
+        @Override
+        public int nextIndex() {
+            return cursor;
+        }
+
+        @Override
+        public int previousIndex() {
+            return cursor - 1;
+        }
+
+        @Override
+        public void remove() {
+            if (lastRet < 0) {
+                throw new IllegalStateException();
+            }
+            ListA.this.remove(lastRet);
+            if (lastRet < cursor) {
+                cursor--;
+            }
+            lastRet = -1;
+        }
+
+        @Override
+        public void set(E e) {
+            if (lastRet < 0) {
+                throw new IllegalStateException();
+            }
+            ListA.this.set(lastRet, e);
+        }
+
+        @Override
+        public void add(E e) {
+            ListA.this.add(cursor, e);
+            cursor++;
+            lastRet = -1;
+        }
+    }
 }
